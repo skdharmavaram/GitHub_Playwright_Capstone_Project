4d350bf0c68101da2acdca541cb6df7d8b350edc
skdharmavaram <dharmavaram.shivakrishna@zensar.com>
2026-02-23 19:37:50 +0530

    myupdate

--- pages/RepoPage.js
+++ pages/RepoPage.js
@@ -4,7 +4,7 @@ export class RepoPage {
 
         // Locators for RepoPage elements
         this.myRepositories = page.getByRole('link', { name: 'My repositories' });
-        this.myRepoName = page.locator('//*[text()="New-Repo-added-for-testing"]');
+        this.myRepoName = page.locator('//*[text()="Playwright_Project_20260127"]');
         this.newRepositoryButton = page.getByText('New repository');
     }
 
